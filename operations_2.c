0f83934574bbf4ad40e2b61e953bbfba179ab53e
Andy <3775@holbertonschool.com>
2022-05-13 00:56:30 +0000

    Add task 7

--- operations_2.c
+++ operations_2.c
@@ -116,3 +116,44 @@ void op_sub(stack_t **stack, unsigned int line_number)
 
 	op_pop(stack, line_number);
 }
+
+
+/**
+ * op_div - divides top from second element and replace both with result
+ * @stack: double pointer to list node
+ * @line_number: line number from monty file
+ *
+ * Return: no return value
+ */
+void op_div(stack_t **stack, unsigned int line_number)
+{
+	stack_t *head, *first, *second;
+	int length, n1, n2, result;
+
+	head = *stack;
+
+	length = get_length(head);
+
+	if (length < 2)
+	{
+		fprintf(stderr, "L%i: can't div, stack too short\n", line_number);
+		a[2] = "div_stack_to_short";
+		return;
+	}
+
+	first = head;
+	second = first->next;
+
+	n1 = first->n;
+	n2 = second->n;
+
+	if (n1 == 0)
+	{
+		fprintf(stderr, "L%i: division by zero\n", line_number);
+	}
+
+	result = n2 / n1;
+	second->n = result;
+
+	op_pop(stack, line_number);
+}
